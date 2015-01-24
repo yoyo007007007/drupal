933239bb8d224818de7a4091544dd42a9a6d5089
yoyo007007007 <yoyo007007007@163.com>
2015-01-24 22:15:24 +0800

    Revert "Revert "测试github操作""
    This reverts commit 118983a43c2de96a37e7ac9ca443ce929fd42d72.

---
+++ qindew/weixin 1.php
@@ -0,0 +1,63 @@
+<?php 
+    include_once 'header.php';
+?>
+<div class="cl">&nbsp;</div>
+		<div id="navigation">
+			<ul>
+			    <li>
+			    	<a href="index.php"><span>首页</span></a>
+			    </li>
+			    <li>
+			    	<a href="about.php"><span>关于我们</span></a>
+			    	<ul>
+			    		<li><a href="about.php">公司简介</a></li>
+			    		<li><a href="team.php">团队介绍</a></li>
+			    	</ul>
+			    </li>
+			    <li>
+			    	<a href="service.php"><span>精品服务</span></a>
+			    	<ul>
+			    		<li><a href="service.php">Java精品课</a></li>
+			    		<li><a href="service.php">物联网金融技术</a></li>
+			    		<li><a href="service.php">高端职业规划</a></li>
+			    	</ul>
+			    </li>
+			    <li>
+			    	<a href="#"><span>团队支持</span></a>			    	
+			    </li>
+			    <li>
+			    	<a href="" class="active"><span>联系我们</span></a>
+			    	<ul>
+			    		<li><a href="contact.php">联系我们</a></li>
+			    		<li><a href="weixin.php">官方微信</a></li>
+			    		
+			    	</ul>
+			    </li>
+			</ul>
+		</div>	
+	</div>
+	<!-- END Logo + Search + Navigation -->
+	<!-- Header -->
+	<div id="header">
+		<!-- Slider -->
+		<div id="slider">
+			<div id="slider-holder">
+				<ul>
+				    <li>
+				    	<div class="slide-info">
+				    		<h2 class="notext txt-we-love-mondays">秦露科技官方微信二维码</h2>
+				    		<p>打开微信，点击右上角的“+”，选择“扫一扫”功能，对准右方二维码即可。</p>				    		
+				    	</div>
+				    	<div class="slide-image">
+				    		<img src="css/images/qinluweixin1.jpg" alt="秦露科技-官方微信" />
+				    	</div>
+				    </li>
+				</ul>
+			</div>
+		</div>
+		<!-- END Slider -->	
+	</div>
+	<!-- END Header -->
+<?php 
+    include_once 'footer.php';
+?>
