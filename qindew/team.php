18941681ecbafc870d6fb8764cc86a2c4bcd9c01
yoyo007007007 <yoyo007007007@163.com>
2015-01-24 22:17:07 +0800

    Revert "Revert "Revert "Revert "测试github操作""""
    This reverts commit aeb36e6437a8b8afa10ff0da9fabcec664718c92.

--- qindew/team.php
+++ qindew/team.php
@@ -5,20 +5,20 @@
 		<div id="navigation">
 			<ul>
 			    <li>
-			    	<a href="index.php"><span>首页</span></a>
+			    	<a hrefsadfasdf"><span>首页</span></a>
 			    </li>
 			    <li>
 			    	<a href="about.php" class="active"><span>关于我们</span></a>
 			    	<ul>
-			    		<li><a href="about.php">公司简介</a></li>
+			    		<li><a href="about.php">公司简�?/a></li>
 			    		<li><a href="team.php">团队介绍</a></li>
 			    	</ul>
 			    </li>
 			    <li>
 			    	<a href="service.php"><span>精品服务</span></a>
 			    	<ul>
-			    		<li><a href="java.php">Java精品课</a></li>
-			    		<li><a href="internet.php">物联网金融技术</a></li>
+			    		<li><a href="java.php">Java精品�?/a></li>
+			    		<li><a href="internet.php">物联网金融技�?/a></li>
 			    		<li><a href="career.php">高端职业规划</a></li>
 			    	</ul>
 			    </li>
@@ -46,7 +46,7 @@
 				    <li>
 				    	<div class="slide-info">
 				    		<h2 class="notext txt-we-love-mondays">秦露科技</h2>
-				    		<p>强大的讲师和咨询师团队针对每个学员做出一对一全面客观的分析。</p>
+				    		<p>强大的讲师和咨询师团队针对每个学员做出一对一全面客观的分析�?/p>
 				    		<a href="#" class="button-more">More</a>
 				    	</div>
 				    	<div class="slide-image">
@@ -56,7 +56,7 @@
 				    <li>
 				    	<div class="slide-info">
 				    		<h2 class="notext txt-we-love-mondays">秦露科技</h2>
-				    		<p>制定出真正属于自己的高端就业方案。</p>
+				    		<p>制定出真正属于自己的高端就业方案�?/p>
 				    		<a href="#" class="button-more">More</a>
 				    	</div>
 				    	<div class="slide-image">
@@ -66,7 +66,7 @@
 				    <li>
 				    	<div class="slide-info">
 				    		<h2 class="notext txt-we-love-mondays">秦露科技</h2>
-				    		<p>我们做的不仅仅是就业 而是适合自己的高薪就业。</p>
+				    		<p>我们做的不仅仅是就业 而是适合自己的高薪就业�?/p>
 				    		<a href="#" class="button-more">More</a>
 				    	</div>
 				    	<div class="slide-image">
@@ -90,9 +90,9 @@
 			<div class="col">
 				<h2>杨治</h2>
 				<h3>职业规划及就业咨询师</h3>
-				<p> 北京理工大学计算机本科
-                                                        精通C++、Oracle、PHP、Java等编程技术
-                                                        曾任职于多家计算机互联网和软件公司
+				<p> 北京理工大学计算机本�?
+                                                        精通C++、Oracle、PHP、Java等编程技�?
+                                                        曾任职于多家计算机互联网和软件公�?
                                                         有着扎实的管理能力和团队领导能力
                                                         以及丰富的职业规划和就业指导经验
                 </p>
@@ -100,54 +100,54 @@
 			<div class="col">
 				<h2> 张东</h2>
 				<h3>职业规划及就业咨询师</h3>
-				<p>北京理工大学计算机本科
-                                                    精通Java、MySQL、Oracle、HTML、JSP等编程技术
+				<p>北京理工大学计算机本�?
+                                                    精通Java、MySQL、Oracle、HTML、JSP等编程技�?
                                                     曾任职于多家软件公司技术主管和项目经理
-                                                    有多年IT行业项目研发和管理经验
+                                                    有多年IT行业项目研发和管理经�?
                                                     并且有着全面的职业规划和就业指导能力
                 </p>
 			</div>
 			<div class="col col-last">
 				<h2> 杨虎</h2>
-				<h3>高级运维工程师</h3>
-				<p>Cisco全球高级认证CCIE高级工程师
-                                                     曾任职于移动金融行业电子商务公司，有多年的大型网络设计、部署、运维经验
+				<h3>高级运维工程�?/h3>
+				<p>Cisco全球高级认证CCIE高级工程�?
+                                                     曾任职于移动金融行业电子商务公司，有多年的大型网络设计、部署、运维经�?
                 </p>
 			</div>
 			
 			
 			<div class="col">
 				<h2>张鹏</h2>
-				<h3> 高级软件工程师</h3>
+				<h3> 高级软件工程�?/h3>
 				<p> 精通Java、Android、等高级编程语言
                                 曾任职于多家软件公司技术主管和项目经理
-                                有多年油田行业IT项目研发和管理经验
+                                有多年油田行业IT项目研发和管理经�?
                 </p>
 			</div>
 			<div class="col">
-				<h2>申蒙蒙</h2>
+				<h2>申蒙�?/h2>
 				<h3>云计算及大数据工程师</h3>
-				<p>中国科学院大学（原中国科学院研究生院）计算机硕士研究生 
-                                精通Java、C/C++等高级编程语言以及Unix/Linux环境下编程
-                                精通云计算与大数据，机器学习、数据挖掘、分布式计算（Hadoop）
+				<p>中国科学院大学（原中国科学院研究生院）计算机硕士研究�?
+                                精通Java、C/C++等高级编程语言以及Unix/Linux环境下编�?
+                                精通云计算与大数据，机器学习、数据挖掘、分布式计算（Hadoop�?
                 </p>
 			</div>
 			<div class="col col-last">
-				<h2>林凤绿</h2>
+				<h2>林凤�?/h2>
 				<h3>高级语言及软件框架师</h3>
 				<p>北京大学计算机硕士研究生
                                                     精通Java、C++、asp.Net等高级编程语言
-                                                    精通Oracle数据库、Shell和 python脚本以及网络安全维护
-                                                    曾经任职华为高级软件工程师负责研发了电信实时计费系统OCS、C++流媒体、SCADA、监控台等相关项目
+                                                    精通Oracle数据库、Shell�?python脚本以及网络安全维护
+                                                    曾经任职华为高级软件工程师负责研发了电信实时计费系统OCS、C++流媒体、SCADA、监控台等相关项�?
                 </p>
 			</div>
 				<div class="col">
 				<h2>杜磊</h2>
-				<h3>互联网金融及算法师</h3>
+				<h3>互联网金融及算法�?/h3>
 				<p>北京大学金融信息技术硕士研究生
                                                     获得IBM DB2高级认证 
                                                     精通Java、Oracle、MySQL、SQLServer等相关语言编程
-                                                    精通互联网金融相关知识及其开发工具
+                                                    精通互联网金融相关知识及其开发工�?
                 </p>
 			</div>
 			<div class="col">
@@ -157,26 +157,26 @@
                                                     北京大学计算机硕士研究生
                                                     精通Java、Oracle等相关的计算机编程语言
                                                     精通各类计算机算法设计和分析以及数学理论基础
-                                                    曾任职于汽车之家（www.autohome.com）负责项目的算法和分析工作
+                                                    曾任职于汽车之家（www.autohome.com）负责项目的算法和分析工�?
                 </p>
 			</div>
 			<div class="col col-last">
-				<h2>朱鹏程  </h2>
+				<h2>朱鹏�? </h2>
 				<h3>游戏设计开发工程师</h3>
-				<p>北京大学软件工程硕士研究生
+				<p>北京大学软件工程硕士研究�?
                                                     北京大学游戏工作室项目负责人
-                                                    精通AndroidApp、SQLite、adb等移动端游戏开发工具
+                                                    精通AndroidApp、SQLite、adb等移动端游戏开发工�?
                                                     曾任职于Forgame集团参与数据采集系统设计与实现、支付中心的建设
                                                     使用LAMP开发体系参与了“全民海贼王”游戏的后台研发工作
                 </p>
 			</div>
             <div class="col">
-				<h2>顾天元</h2>
-				<h3>产品设计研发工程师</h3>
-				<p>本科毕业于北京航空航天大学计算机科学与技术专业
-                                                    硕士研究生毕业于美国康奈尔大学人机交互专业
+				<h2>顾天�?/h2>
+				<h3>产品设计研发工程�?/h3>
+				<p>本科毕业于北京航空航天大学计算机科学与技术专�?
+                                                    硕士研究生毕业于美国康奈尔大学人机交互专�?
                                                     曾任职于聚美优品、爱奇艺、京东等公司
-                                                    担任产品研发设计工程师，产品经理等职务
+                                                    担任产品研发设计工程师，产品经理等职�?
                 </p>
 			</div>
 			<div class="cl">&nbsp;</div>
